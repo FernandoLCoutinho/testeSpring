f3e0e220430637a66360717678476d6f744943cb
geoinformacao <geoinformacao@CAS554909W10D02.senacsp.edu.br>
2019-05-09 20:03:31 -0300

    Teste do projeto inicial

---
+++ src/main/java/br/acaitech/springMVC/FluxoDeVenda/FluxoDeVendaApplication.java
@@ -0,0 +1,13 @@
+package br.acaitech.springMVC.FluxoDeVenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxoDeVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxoDeVendaApplication.class, args);
+	}
+
+}
